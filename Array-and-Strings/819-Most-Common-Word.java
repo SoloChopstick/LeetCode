5b9a84a8e9f1c442fa121ccda5071d1f49a4aeea
Han Wen Xie <hanwenxie@hotmail.com>
2019-05-16 22:09:43 -0400

    Added comments to 819

--- Array-and-Strings/819-Most-Common-Word.java
+++ Array-and-Strings/819-Most-Common-Word.java
@@ -3,7 +3,9 @@ class Solution {
         //preprocessing
         paragraph = paragraph.toLowerCase().replaceAll("\\[W+]"," ");
         //paragraph = paragraph.replace("."," ").replace(","," ").replace("!"," ").replace("'"," ").replace(";"," ").replace("?"," ");
-        System.out.println(paragraph);
+        //System.out.println(paragraph);
+		
+		//Adding banned list into a set
         Set <String> bannedWords = new HashSet<>();
         for (int i = 0; i < banned.length; i++) {
             bannedWords.add(banned[i]);
@@ -11,6 +13,7 @@ class Solution {
             
         HashMap <String, Integer> map = new HashMap <>();
         int i = 0;
+		//generating all Strings and keeping track of recurrence of non-banned words
         while (i < paragraph.length()) {
             StringBuilder bs = new StringBuilder();
             while (i < paragraph.length() && paragraph.charAt(i) != ' ') {
@@ -28,6 +31,7 @@ class Solution {
         }
         int max = 0;
         String answer = "";
+		//loop to find most common word in map
         for (String str : map.keySet()) {
             int value = map.get(str);
             if (value > max) {
