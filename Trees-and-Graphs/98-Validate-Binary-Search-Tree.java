f546baffa18d9d6ec437db11a48c59ef5735e66f
Han Wen Xie <hanwenxie@hotmail.com>
2019-05-16 23:33:15 -0400

    Adding recursive solution to 98

---
+++ Trees-and-Graphs/98-Validate-Binary-Search-Tree.java
@@ -0,0 +1,28 @@
+/*
+Solution 1: Recursion
+Time: O(n)
+Space: O(n)
+*/
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper (root, null, null);
+    }
+    
+    public boolean helper (TreeNode node, Integer min, Integer max) {
+        //base case
+        if (node == null) {
+            return true;
+        }
+        //Bad cases
+        if (min != null && node.val <= min) {
+            return false;
+        }
+        if (max != null && node.val >= max) {
+            return false;
+        }
+        else {
+            return helper(node.left, min, node.val) && helper(node.right, node.val, max);
+        }
+    }
+}
