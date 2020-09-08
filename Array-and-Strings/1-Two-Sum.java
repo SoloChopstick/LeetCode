9a34894d3eab3bc7d3ef683f3f231bb832c63a34
Han Wen Xie <hanw.xie@gmail.com>
2020-09-07 23:39:26 -0400

    Added solutions to 1-Two-Sum

---
+++ Array-and-Strings/1-Two-Sum.java
@@ -0,0 +1,31 @@
+//O(n) time, O(n) space
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+/*
+//Brute Force : O(n^2 time), O(1) space
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        //not found
+        return new int[]{-1, -1};
+    }
+}
+*/
